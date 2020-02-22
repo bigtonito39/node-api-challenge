5a851f272e9ab092812f175a5c761601b792a26d
bigtonito39 <bigtonito@hotmail.com>
2020-02-21 23:43:42 -0500

    delete, update and posting funtionality for a project all working now using middleware

--- projects/projects.js
+++ projects/projects.js
@@ -41,16 +41,58 @@ router.get("/:id", validateProjectID(), (req, res, next) => {
 // })
 //..............................................................ENDS HERE
 
-
+//-------------------POST to create a project
 router.post("/", validateProjectBody(), (req, res, next) =>{
     projectDB.insert(req.body)
     .then(projecBody => {
         res.status(201).json(projecBody)
     })
-    .catch(next)
+    .catch( error => {
+        next(error)
+    })
 
 })
+//-----------------------------------------------ENDS HERE
 
+//---------------------------------DELETE A PROJECT
+
+router.delete("/:id",validateProjectID(), (req, res, next) => {
+    projectDB.remove(req.params.id)
+    .then(count => {
+        //as long id is greater than 0 and a valid id is given this will run
+        if (count > 0){
+            res.status(200).json({
+            message:"Project has been deleted"})
+        }
+        
+    })
+    .catch( error => {
+        next(error)
+    })
+
+} )
+//-----------------------------------------------------------Ends here
+
+//-----------updates a project
+
+router.put("/:id",validateProjectBody(),validateProjectID(), (req, res, next) => {
+    projectDB.update(req.params.id, req.body)
+    .then (updatedProject => {
+        if (updatedProject) {
+            res.status(200).json(updatedProject)
+        }else{
+            res.status(404).json({
+                message:"The project could not be found"
+            })
+        }
+    
+    })
+      .catch(error=> {
+          next(error)
+      })
+
+} )
+//---------------------ends here
 
 module.exports = router
 
